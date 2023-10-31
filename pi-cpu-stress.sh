43847b08148101caa9588273b9c5cf46899b94ac
Geekworm.com <46470628+geekworm-com@users.noreply.github.com>
2023-10-31 18:06:25 +0800

    Update pi-cpu-stress.sh

--- pi-cpu-stress.sh
+++ pi-cpu-stress.sh
@@ -7,7 +7,7 @@
 # Variables.
 test_run=1
 test_results_file="${HOME}/cpu_temp_$test_run.log"
-stress_length="10m"
+stress_length="20m"
 
 # Verify stress-ng is installed.
 if ! [ -x "$(command -v stress-ng)" ]; then
@@ -20,35 +20,31 @@ printf "Logging temperature and throttling data to: $test_results_file\n"
 
 # Start logging temperature data in the background.
 # 当前的秒数；
-current_second=0
+seconds=0
 while /bin/true; do
   # Print the date (e.g. "Wed 13 Nov 18:24:45 GMT 2019") and a tab.
   # 打印当前日期
-  # date | tr '\n' '\t' >> $test_results_file;  
-  # 打印当前的秒数, -n 表示不换行
-  printf $current_second >> $test_results_file;
-  printf '\t' >> $test_results_file;
-  # 打印当前的秒数, -n 表示不换行
-  # echo -n $current_second >> $test_results_file;
-  # echo '\t' >> $test_results_file;
-
+  date | tr '\n' '\t' >> $test_results_file;  
+  
+  # 打印当前的秒数,  added by Geekworm
+  printf '%d\t' $seconds >> $test_results_file;
+  
   # Print the temperature (e.g. "39.0") and a tab.
-  # vcgencmd measure_temp | tr -d "temp=" | tr -d "'C" | tr '\n' '\t' >> $test_results_file;
-  # 打印CPU的温度
-  vcgencmd measure_temp | tr -d "temp=" | tr '\n' '\t' >> $test_results_file;
-
+  vcgencmd measure_temp | tr -d "temp=" | tr -d "'C" | tr '\n' '\t' >> $test_results_file;
+  
   # Print the throttle status (e.g. "0x0") and a tab.
   # 检查是否出现欠压现象
   # 这个数字的第 0 位为 1 的话，表明当前发生了输入电压不足的情况；
   # 这个数字的第 16 位为 1 的话，表明启动之后曾经发生过输入电压不足的情况；
-  # vcgencmd get_throttled | tr -d "throttled=" | tr '\n' '\t' >> $test_results_file;
+  vcgencmd get_throttled | tr -d "throttled=" | tr '\n' '\t' >> $test_results_file;
 
   # Print the current CPU frequency.
   # 打印当前CPU时钟
-  # vcgencmd measure_clock arm | sed 's/^.*=//' >> $test_results_file;
-  
-  let current_second=current_second+60;
-  sleep 60;  
+  vcgencmd measure_clock arm | sed 's/^.*=//' >> $test_results_file;
+
+  # Aded by Geekworm
+  let seconds=seconds+30;
+  sleep 30;  
 done &
 
 # Store the logging pid.
